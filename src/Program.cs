a6457b39265f7219ec7c411ca5b93005aa129553
neko3cs <minaton.0722.net@gmail.com>
2020-08-28 17:34:03 +0900

    support DES (but not implemented yet)

--- src/Program.cs
+++ src/Program.cs
@@ -7,19 +7,24 @@ namespace CryptStr
 {
     class Program
     {
-        static void Main(string[] args)
-        {
-            CoconaApp.Run<Program>(args);
-        }
+        private const string DefaultAlgorithms = "TripleDES";
+
+        static void Main(string[] args) => CoconaApp.Run<Program>(args);
 
         [Command(Description = "Encrypt string of value.")]
         public int Enc(
             [Argument] string value,
             [Option('k')] string key,
-            [Option('v')] string iv
+            [Option('v')] string iv,
+            [Option('a')][Algorithms] string algorithms = DefaultAlgorithms
         )
         {
-            var cryptor = new TripleDESCryptor(key, iv);
+            ICryptor cryptor = algorithms switch
+            {
+                nameof(SupportAlgorithms.TripleDES) => new TripleDESCryptor(key, iv),
+                nameof(SupportAlgorithms.DES) => new DESCryptor(key, iv),
+                _ => throw new ArgumentException()
+            };
             Console.WriteLine(cryptor.Encrypt(value));
 
             return 0;
@@ -29,19 +34,33 @@ namespace CryptStr
         public int Dec(
             [Argument] string value,
             [Option('k')] string key,
-            [Option('v')] string iv
+            [Option('v')] string iv,
+            [Option('a')][Algorithms] string algorithms = DefaultAlgorithms
         )
         {
-            var cryptor = new TripleDESCryptor(key, iv);
+            ICryptor cryptor = algorithms switch
+            {
+                nameof(SupportAlgorithms.TripleDES) => new TripleDESCryptor(key, iv),
+                nameof(SupportAlgorithms.DES) => new DESCryptor(key, iv),
+                _ => throw new ArgumentException()
+            };
             Console.WriteLine(cryptor.Decrypt(value));
 
             return 0;
         }
 
         [Command(Description = "Generate key and IV to file.")]
-        public void Gen()
+        public void Gen(
+            [Option('a')][Algorithms] string algorithms = DefaultAlgorithms
+        )
         {
-            var keyAndIV = TripleDESCryptor.Generate();
+
+            var keyAndIV = algorithms switch
+            {
+                nameof(SupportAlgorithms.TripleDES) => TripleDESCryptor.Generate(),
+                nameof(SupportAlgorithms.DES) => DESCryptor.Generate(),
+                _ => throw new ArgumentException()
+            };
             File.WriteAllText(
                 Path.Combine(Directory.GetCurrentDirectory(), "cryptstr.json"),
                 JsonConvert.SerializeObject(new
